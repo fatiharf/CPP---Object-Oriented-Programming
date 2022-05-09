0f603b7ea3add96d987c60d09256aa24411ea7e6
Fatih Akbulut <fatihakbulut997@gmail.com>
2022-05-09 22:09:14 +0300

    Giriş Bilgileri yazıldı.

---
+++ giris.cpp
@@ -0,0 +1,15 @@
+/*
+CPP ÝLE  NESNE YÖNELÝMLÝ PROGRAMLAMA(OBJECT ORÝENTED PROGRAMMÝNG -OOP)
+
+* C++ C dilinden türetilmiþ orta seviyeli bir programlama dilidir. 
+
+* C dilinden farký NYP desteðinin olmasýdýr.
+
+*NYP desteði sayesinde modern programlamaya imkan vermesinden dolayý Sürücü yazýlýmlarý, Oyun, Görüntü iþleme,
+ Ýþletim sistemleri gibi hýzýn önemli olduðu yerlerde kullanýlýr.
+
+*Diðer programlama dillerinin sözdizimi ile benzerlik gösterdiði için bu dili öðrenmek sonraki programlama dillerinde iþimizi 
+kolaylaþtýracaktýr.
+
+** Bu notlarda sýfýrdan "CPP Ýle Nesne Yönelimli Programlama" konularýný kod ekranýnda çeþitli yorumlarýmla birlikte anlatarak
+paylaþacaðým.
