ef64ec3a03e1ab44737242121d37adfd47a35253
Fatih Akbulut <fatihakbulut997@gmail.com>
2022-05-10 13:16:13 +0300

    Genel C++ komutları ve cout komutu ile ekrana 'Hello World' yazdırlması.

---
+++ ilkprogram.cpp
@@ -0,0 +1,34 @@
+
+// Genel C++ komutlarý ve cout komutu kullanarak konsola "Hello World" yazdýrýlmasý.
+
+
+#include<iostream>	 /* #include komutunu  standart veya sonradan kullanýcý tarafýndan oluþturulmuþ veri ve fonksiyonlarý
+							programa dahil etmek için kullanýrýnýz.
+						
+						#include<iostream> komutunu kullanarak da programa "iostream"  standart kütüphanesini programmýmýza 
+						tanýmlarýz.Çalýþan her programda bulumasý gerekir!
+						
+						Bu kütüphane ile program çalýþtýðýnda; çýktý alabiliriz veya programa girdiler verebiliriz. */
+ 
+ 
+ 
+using namespace std; 	/* using namespace std; deyimi standart kütüphanenin bütün bileþenlerini kapsam dahiline sokar.Çalýþan
+						   her programda bulunmasý gerekir! */
+
+
+
+int main(){				 	 /* int main(){ } fonksiyonu programýn ana fonksiyonudur programýn çalýþtýrýlmaya baþlandýðý
+							 noktadýr.Çalýþan her programda bulumasý gerekir. */
+	
+cout<<"Hello World";	 	/* Cout komutunu  kullanarak ekrana çýktýlarý yazdýrabiliriz ,sonundaki noktalý virgüle dikkat 
+								edilmelidir.
+							
+						   		Bu komut ile ekrana Hello World yazdýrýlýr.
+							*/
+	
+	
+	
+	
+	
+	return 0;			/* Programýn hatasýz sonlandýrýldýðýný  belirtmek için kullanýlýr.*/
+}
